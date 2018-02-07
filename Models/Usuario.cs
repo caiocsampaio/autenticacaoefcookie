bf5ace2d3ce0315a0da8af4b488d6a39b572dbe2
JAIR AFONSO DE OLIVEIRA <jair.afonso@gmail.com>
2018-02-07 19:48:49 -0200

    first commit

---
+++ Models/Usuario.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace autenticacaoefcookie.Models
+{
+    public class Usuario
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdUsuario { get; set; }
+
+        [StringLength(100, MinimumLength = 4)]
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 4)]
+        public string Email { get; set; }
+
+        
+        [Required]
+        [StringLength(12, MinimumLength = 4)]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        public ICollection<UsuarioPermissao> UsuariosPermissoes { get; set; }
+
+    }
+}
